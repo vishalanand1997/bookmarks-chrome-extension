f97f6dcef5b19ae6e0fc68004d42cc75943aa29c
devsignity <>
2022-01-24 20:03:40 +0530

    Added the cropper on image screenshort

--- src/pages/Screen/ScreenShort.jsx
+++ src/pages/Screen/ScreenShort.jsx
@@ -1,16 +1,165 @@
-import React from 'react';
-
+import React, { useState, useCallback } from 'react';
+import "./ScreenShort.css";
+import Cropper from 'react-easy-crop'
 function ScreenShort() {
     const [screenshort, setScreenshort] = React.useState("")
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [zoom, setZoom] = useState(1);
+    const [rotation, setRotation] = useState(0)
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+    const [croppedImage, setCroppedImage] = useState(null)
+
+    const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+        setCroppedAreaPixels(croppedAreaPixels)
+        console.log("On CropComplete", croppedArea, croppedAreaPixels)
+    }, [])
     React.useEffect(() => {
         chrome.storage.local.get("data", (data) => {
             setScreenshort(JSON.parse(data.data))
             console.log("SCREENSHORT", data.data);
         })
     }, [])
-    return <div>ScreenShort
-        <img id="target" src={screenshort} height="100%" width="100%" />
-    </div>;
+
+    const showCroppedImage = useCallback(async () => {
+        try {
+            const croppedImage = await getCroppedImg(
+                screenshort,
+                croppedAreaPixels,
+                rotation
+            )
+            console.log('donee', { croppedImage })
+            setCroppedImage(croppedImage)
+        } catch (e) {
+            console.error(e)
+        }
+    }, [croppedAreaPixels, rotation])
+    const createImage = (url) =>
+        new Promise((resolve, reject) => {
+            const image = new Image()
+            image.addEventListener('load', () => resolve(image))
+            image.addEventListener('error', (error) => reject(error))
+            image.setAttribute('crossOrigin', 'anonymous') // needed to avoid cross-origin issues on CodeSandbox
+            image.src = url
+        })
+
+    function getRadianAngle(degreeValue) {
+        return (degreeValue * Math.PI) / 180
+    }
+
+    /**
+     * Returns the new bounding area of a rotated rectangle.
+     */
+    function rotateSize(width, height, rotation) {
+        const rotRad = getRadianAngle(rotation)
+
+        return {
+            width:
+                Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
+            height:
+                Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
+        }
+    }
+
+    /**
+     * This function was adapted from the one in the ReadMe of https://github.com/DominicTobias/react-image-crop
+     */
+    async function getCroppedImg(
+        imageSrc,
+        pixelCrop,
+        rotation = 0,
+        flip = { horizontal: false, vertical: false }
+    ) {
+        const image = await createImage(imageSrc)
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')
+
+        if (!ctx) {
+            return null
+        }
+
+        const rotRad = getRadianAngle(rotation)
+
+        // calculate bounding box of the rotated image
+        const { width: bBoxWidth, height: bBoxHeight } = rotateSize(
+            image.width,
+            image.height,
+            rotation
+        )
+
+        // set canvas size to match the bounding box
+        canvas.width = bBoxWidth
+        canvas.height = bBoxHeight
+
+        // translate canvas context to a central location to allow rotating and flipping around the center
+        ctx.translate(bBoxWidth / 2, bBoxHeight / 2)
+        ctx.rotate(rotRad)
+        ctx.scale(flip.horizontal ? -1 : 1, flip.vertical ? -1 : 1)
+        ctx.translate(-image.width / 2, -image.height / 2)
+
+        // draw rotated image
+        ctx.drawImage(image, 0, 0)
+
+        // croppedAreaPixels values are bounding box relative
+        // extract the cropped image using these values
+        const data = ctx.getImageData(
+            pixelCrop.x,
+            pixelCrop.y,
+            pixelCrop.width,
+            pixelCrop.height
+        )
+
+        // set canvas width to final desired crop size - this will clear existing context
+        canvas.width = pixelCrop.width
+        canvas.height = pixelCrop.height
+
+        // paste generated rotate image at the top left corner
+        ctx.putImageData(data, 0, 0)
+
+        // As Base64 string
+        // return canvas.toDataURL('image/jpeg');
+
+        // As a blob
+        return new Promise((resolve, reject) => {
+            canvas.toBlob((file) => {
+                resolve(URL.createObjectURL(file))
+            }, 'image/jpeg')
+        })
+    }
+
+
+    return (
+        <div className="App">
+            <div className="crop-container">
+                <Cropper
+                    image={screenshort}
+                    crop={crop}
+                    zoom={zoom}
+                    aspect={4 / 3}
+                    onCropChange={setCrop}
+                    onCropComplete={onCropComplete}
+                    onZoomChange={setZoom}
+                />
+            </div>
+            <div className="controls">
+                <input
+                    type="range"
+                    value={zoom}
+                    min={1}
+                    max={3}
+                    step={0.1}
+                    aria-labelledby="Zoom"
+                    onChange={(e) => {
+                        setZoom(e.target.value)
+                    }}
+                    className="zoom-range"
+                />
+                <button
+                    onClick={showCroppedImage}
+                >
+                    Show Result
+                </button>
+            </div>
+        </div>);
 }
 
 export default ScreenShort;

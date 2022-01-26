bed6a7deea5398b545faa10fa4f6dfa663a2838d
Deepak Mandal <deepak379mandal@gmail.com>
2022-01-26 12:00:59 +0530

    create note creation setup

--- src/pages/CreateNotes/CreateNotes.jsx
+++ src/pages/CreateNotes/CreateNotes.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Card, Row, Col, Divider, Input } from 'antd';
 
-const { Title } = Typography;
-const { TextArea } = Input;
 
 export default function CreateNotes() {
     const [state, setState] = useState({
@@ -16,19 +13,9 @@ export default function CreateNotes() {
 
     }, [])
 
-    return (
-        <Row style={{ textAlign: "center" }}>
-            <Col span={12}>
-                <Title>
-                    create Notes
-                </Title>
-                <Divider dashed />
-                <Card title="new note">
-                    <Input value={state.title} placeholder="Note title" style={{ display: "block" }} readOnly />
-                    <textarea value={state.note} readOnly cols={40} rows={15}></textarea>
-                    <button>Add Note</button>
-                </Card>
-            </Col>
-        </Row>
-    );
+    return <div>
+        <h2>
+        Create new Note
+        </h2>
+    </div>;
 }
